f2f4d1b791d0f1236c9e71b9bf4ec2c9730b5e72
Manish Kumar <37174461+Micky143@users.noreply.github.com>
2018-09-19 16:10:53 +0530

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,24 @@
+"""
+------------------------------------------------------
+|            Password Generator   1             |
+------------------------------------------------------
+import random
+
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passlen = 8
+p =  "".join(random.sample(s,passlen ))
+print(p)
+"""
+
+"""
+------------------------------------------------------
+|            Password Generator   2             |
+------------------------------------------------------
+import string
+import random
+
+def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+print(pw_gen(int(input('How many characters in your password?'))))
+"""
